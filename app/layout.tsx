6dc9845d15535084de034b0a4d47fca7ccbcb1c1
Ashwin Torphe <ashwin.torphe@gmail.com>
2026-01-08 03:22:28 +0530

    new meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,28 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Mediant",
-  description: "Music Visualizer Experience",
+  description:
+    "MEDIANT Releases on Labels - Pleasure Records, Deepsessions Recordings.",
+  openGraph: {
+    title: "Mediant",
+    description:
+      "MEDIANT Releases on Labels - Pleasure Records, Deepsessions Recordings.",
+    images: [
+      {
+        url: "/meta.png",
+        width: 1200,
+        height: 630,
+        alt: "Mediant Preview",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mediant",
+    description:
+      "MEDIANT Releases on Labels - Pleasure Records, Deepsessions Recordings.",
+    images: ["/meta.png"],
+  },
 };
 
 export default function RootLayout({
